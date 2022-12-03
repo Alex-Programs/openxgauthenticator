b531073e98986ec063da65b6c350a03988a26b97
AlexAndHisScripts <alexcj2802@gmail.com>
2022-12-03 20:31:08 +0000

    Added new download method that circumvents fw

--- openxgauthenticator/site/main.py
+++ openxgauthenticator/site/main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request, Response
 import waitress
+import base64
+import hashlib
 
 app = Flask(__name__)
 
@@ -15,4 +17,25 @@ def download(filename):
     return send_file("downloads/" + filename, as_attachment=True)
 
 
-waitress.serve(app, port=4264)
+def obfuscated_download(filename):
+    yield "<html><head><title>Definitely HTML</title></head><body><h1>Hello! This is totally real HTML.</h1>"
+
+    with open("downloads/" + filename, "rb") as f:
+        data = f.read()
+        encoded = base64.encodebytes(data).decode("utf-8")
+        yield encoded
+
+    yield "</body></html>"
+
+@app.route("/circumvent/<path:OS>")
+def circumvent(OS):
+    if OS == "windows":
+        return Response(obfuscated_download("openxgauthenticator.exe"), mimetype="text/html")
+    elif OS == "linux":
+        return Response(obfuscated_download("openxgauthenticator-linux"), mimetype="text/html")
+    else:
+        return Response(obfuscated_download("openxgauthenticator-linux"), mimetype="text/html")
+
+port = 4264
+print(f"Serving on port {str(port)}")
+waitress.serve(app, port=port)
